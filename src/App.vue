60dd0ac5dffc5621a5b5b73c9148d9bc5d256d5b
Eligio Biasi <125873472+EligioBiasi@users.noreply.github.com>
2023-09-07 16:51:58 +0200

    Initialize Add-footer-in-App.vue

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script>
+import AppHeader from './components/AppHeader.vue';
+// import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
+
+export default{
+    name: 'App',
+    components:{
+    // AppMain,
+    AppHeader,
+    AppFooter,
+}
+  }
+</script>
+
+<template>
+  <header>
+    <AppHeader />
+  </header>
+  <!-- <AppMain /> -->
+<main>
+
+  <router-view>
+
+  </router-view>
+</main>
+<footer>
+    <AppFooter/>
+</footer>
+</template>
+
+<style lang="scss">
+@use './styles/general.scss' as *;
+
+</style>
